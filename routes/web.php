89da917318833241afe2415a77a101c704c3353d
dinushchathurya <dinushchathurya21@gmail.com>
2021-09-04 14:59:26 +0530

    :sparkles: Add endpoint for upload files

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FileController;
 
 Route::get('/', [FileController::class, 'index']);
-
+Route::post('dropzone/store', [FileController::class, 'store'])->name('dropzone.store');;
