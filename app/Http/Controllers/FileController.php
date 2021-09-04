237a6bbd040ede51125c294060f98af4ce1f6b34
dinushchathurya <dinushchathurya21@gmail.com>
2021-09-04 15:00:07 +0530

    :sparkles: Create function to upload files

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -10,4 +10,14 @@ class FileController extends Controller
     {
         return view('welcome');
     }
+
+    public function store(Request $request)
+    {
+        $image = $request->file('file');
+
+        $imageName = time().'.'.$image->extension();
+        $image->move(public_path('images'),$imageName);
+   
+        return response()->json(['success'=>$imageName]);
+    }
 }
